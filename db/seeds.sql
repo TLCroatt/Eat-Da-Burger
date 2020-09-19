c54465c9b5f24b713f02f85664ad0f0a175f4bbb
TLCroatt <trishacroatt@gmail.com>
2020-09-19 15:53:46 -0600

    add .sql files, add orm.js and connection.js

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger (burger_name) VALUES ("Hamburger");
+INSERT INTO burger (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burger (burger_name) VALUES ("BBQ Burger");
+INSERT INTO burger (burger_name) VALUES ("Veggie Burger");
