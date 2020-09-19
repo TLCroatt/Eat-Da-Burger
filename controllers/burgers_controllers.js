614da04f2c01f24a182ccb45faabfafadb3b053b
TLCroatt <trishacroatt@gmail.com>
2020-09-19 16:17:15 -0600

    add burgers_controllers.js

---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,51 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model (cat.js) to use its database functions.
+var cat = require("../models/burger.js");
+const burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  cat.all(function(data) {
+    // data is our result - our array of burgers
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+    res.redirect("/");
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured
+    },
+    condition, function() {
+     res.redirect("/")
+    }
+  );
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function() {
+        res.redirect("/");
+    })
+})
+
+// Export routes for server.js to use.
+module.exports = router;
