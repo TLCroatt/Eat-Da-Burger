d552c37c8dbf8d0fafbc842eac7530b2d0a81a8b
TLCroatt <trishacroatt@gmail.com>
2020-09-19 16:01:25 -0600

    add models/burger.js

--- db/schema.sql
+++ db/schema.sql
@@ -4,10 +4,10 @@ CREATE DATABASE burgers_db;
 
 USE burgers_db;
 
-CREATE TABLE burger
+CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+	name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
